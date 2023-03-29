6e363efe3df04f1163c21166e00bc4fd0e8c244a
Sylvain Roux <fafmio@outlook.fr>
2023-03-29 17:33:19 +0200

    Started comments

---
+++ src/main/java/com/bezkoder/springjwt/models/Comment.java
@@ -0,0 +1,47 @@
+package com.bezkoder.springjwt.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @NotNull
+    @Size(max = 500)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY, targetEntity = User.class, optional = false)
+    private User author;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+}
